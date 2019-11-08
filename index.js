27fca7a2842c6911be91508cf5b84363709abf25
Jan Amann <jan@amann.me>
2019-11-08 12:00:23 +0100

    Add logging and PR workflow.

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const validatePrTitle = require('./src/validatePrTitle');
 
 async function run() {
   try {
-    await validatePrTitle(github.context.payload.pull_request.title);
+    const client = new github.GitHub(process.env.GITHUB_TOKEN);
+    const {data: pullRequest} = client.pulls.get({
+      url: github.context.payload.pull_request.url
+    });
+    throw new Error(JSON.stringify(pullRequest));
+
+    // TODO: This isn't up to date. use rest api?
+    await validatePrTitle(pullRequest.title);
   } catch (error) {
     core.setFailed(error.message);
   }
