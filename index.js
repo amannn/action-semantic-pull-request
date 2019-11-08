3d42fccbde7c82eaa400659eaf6c26990700c63d
Jan Amann <jan@amann.me>
2019-11-08 10:23:13 +0100

    fix: Log to stderr.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const parser = require('conventional-commits-parser');
 
-console.log(JSON.stringify(github.context.payload.pull_request));
+// console.log(JSON.stringify(github.context.payload.pull_request));
 // function parser (raw, options, regex) {
 
-core.setFailed('Action failed with error`);
+core.setFailed(JSON.stringify(github.context.payload.pull_request));
