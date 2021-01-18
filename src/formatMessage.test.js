e8ecc03cc1c9f9de7164f3bac5986d3198d5fda2
Jan Amann <jan@amann.me>
2021-01-18 14:46:48 +0100

    Update src/formatMessage.test.js

--- src/formatMessage.test.js
+++ src/formatMessage.test.js
@@ -1,23 +1,29 @@
 const formatMessage = require('./formatMessage');
 
-describe('parse pattern error', () => {
-  it('parse string without changes if template values not provided', () => {
-    const customError = 'this is test';
-    expect(formatMessage(customError)).toEqual(customError);
-  });
+it('handles a string without variables', () => {
+  const message = 'this is test';
+  expect(formatMessage(message)).toEqual(message);
+});
 
-  it('parses string properly by replacing one template value', () => {
-    expect(
-      formatMessage('this is {subject} test', {subject: 'my subject'})
-    ).toEqual('this is my subject test');
-  });
+it('replaces a variable', () => {
+  expect(
+    formatMessage('this is {subject} test', {subject: 'my subject'})
+  ).toEqual('this is my subject test');
+});
 
-  it('parses string properly by replacing both template values', () => {
-    expect(
-      formatMessage('this {title} is {subject} test', {
-        subject: 'my subject',
-        title: 'my title'
-      })
-    ).toEqual('this my title is my subject test');
-  });
+it('replaces multiple variables', () => {
+  expect(
+    formatMessage('this {title} is {subject} test', {
+      subject: 'my subject',
+      title: 'my title'
+    })
+  ).toEqual('this my title is my subject test');
+});
+
+it('replaces multiple instances of a variable', () => {
+  expect(
+    formatMessage('99 bottles of {beverage} on the wall, 99 bottles of {beverage}.', {
+      beverage: 'beer'
+    })
+  ).toEqual('99 bottles of beer on the wall, 99 bottles of beer.');
 });
