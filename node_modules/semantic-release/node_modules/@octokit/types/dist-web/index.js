77a5839bb18a83f4ce20b483269220135fd4cd15
Jan Amann <jan@amann.me>
2021-01-19 08:47:31 +0100

    chore: Compile dependencies (#77)
    * Compile dependencies
    
    * Build in current branch
    
    * Check in dist (for publishing in master)

--- node_modules/semantic-release/node_modules/@octokit/types/dist-web/index.js
+++
@@ -1,4 +0,0 @@
-const VERSION = "4.1.10";
-
-export { VERSION };
-//# sourceMappingURL=index.js.map
--- node_modules/semantic-release/node_modules/@semantic-release/github/lib/definitions/constants.js
+++
@@ -1,5 +0,0 @@
-const ISSUE_ID = '<!-- semantic-release:github -->';
-
-const RELEASE_NAME = 'GitHub release';
-
-module.exports = {ISSUE_ID, RELEASE_NAME};
